aa2209f2c186586ddc66a2b50530a1f25de4b06b
Olivier Sallou <olivier.sallou@irisa.fr>
2016-09-28 09:54:59 +0200

    fix consul check

--- biomaj_process/biomaj_process_web.py
+++ biomaj_process/biomaj_process_web.py
@@ -51,11 +51,15 @@ def start_server(config):
     if config['consul']['host']:
         consul_agent = consul.Consul(host=config['consul']['host'])
         consul_agent.agent.service.register('biomaj_process', service_id=config['consul']['id'], port=config['web']['port'], tags=['biomaj'])
-        check = consul.Check.http(url=config['web']['local_endpoint'], interval=20)
+        check = consul.Check.http(url=config['web']['local_endpoint'] + '/api/process', interval=20)
         consul_agent.agent.check.register(config['consul']['id'] + '_check', check=check, service_id=config['consul']['id'])
 
     app.run(host='0.0.0.0', port=config['web']['port'], ssl_context=context, threaded=True, debug=config['web']['debug'])
 
+
+@app.route('/api/process', methods=['GET'])
+def ping():
+    return jsonify({'msg': 'pong'})
 
 @app.route('/api/process/metrics', methods=['GET'])
 def metrics():
