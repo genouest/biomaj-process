25ec218fd655fc4733b69761330fc435eb881f35
Olivier Sallou <olivier.sallou@irisa.fr>
2017-01-03 11:08:57 +0100

    do not start web thread if consul not defined

--- biomaj_process/process_service.py
+++ biomaj_process/process_service.py
@@ -50,6 +50,9 @@ def consul_declare(config):
             check=check,
             service_id=config['consul']['id']
         )
+        return True
+    else:
+        return False
 
 
 class ProcessService(object):
@@ -66,9 +69,9 @@ class ProcessService(object):
             self.config = yaml.load(ymlfile)
             Utils.service_config_override(self.config)
 
-        consul_declare(self.config)
-        web_thread = threading.Thread(target=start_web, args=(self.config,))
-        web_thread.start()
+        if consul_declare(self.config):
+            web_thread = threading.Thread(target=start_web, args=(self.config,))
+            web_thread.start()
 
         Zipkin.set_config(self.config)
 
