656b87ee67995c303843b4e76a598b9079f95ea8
Olivier Sallou <olivier.sallou@irisa.fr>
2017-03-02 12:22:55 +0100

    retry connection in case of contact failure with proxy

--- biomaj_process/process_client.py
+++ biomaj_process/process_client.py
@@ -34,13 +34,19 @@ class ProcessServiceClient(object):
         if not self.remote:
             self.session = str(uuid.uuid4())
             return self.session
-        r = requests.post(proxy + '/api/process/session/' + bank)
-        if not r.status_code == 200:
-            raise Exception('Failed to connect to the download proxy')
-        result = r.json()
-        self.session = result['session']
-        self.proxy = proxy
-        return result['session']
+
+        for i in range(3):
+            try:
+                url = proxy + '/api/process/session/' + bank
+                r = requests.post(url)
+                if r.status_code == 200:
+                    result = r.json()
+                    self.session = result['session']
+                    self.proxy = proxy
+                    return result['session']
+            except Exception:
+                logging.exception('Failed to send create operation: %s' % (url))
+        raise Exception('Failed to connect to the process proxy')
 
     def execute_process(self, biomaj_process):
         if self.remote:
@@ -60,11 +66,23 @@ class ProcessServiceClient(object):
         exitcode = -1
         info = None
         logging.info("Process:RemoteProcess:Waiting")
+        errors = 0
         while not over:
-            r = requests.get(self.proxy + '/api/process/session/' + self.bank + '/' + self.session)
-            if not r.status_code == 200:
-                raise Exception('Failed to connect to the download proxy')
-            result = r.json()
+            if errors >= 3:
+                raise Exception('Failed to contact process proxy 3 times, stopping...')
+            result = {'exitcode': -1}
+            try:
+                r = requests.get(self.proxy + '/api/process/session/' + self.bank + '/' + self.session)
+                if not r.status_code == 200:
+                    logging.error('Failed to connect to the process proxy')
+                    errors += 1
+                else:
+                    result = r.json()
+                    errors = 0
+            except Exception:
+                logging.exception('Failed to get status from process proxy')
+                errors += 1
+
             # {'error': error, 'exitcode': exitcode, 'info': info}
             if result['exitcode'] > -1:
                 exitcode = result['exitcode']
@@ -78,6 +96,12 @@ class ProcessServiceClient(object):
 
     def clean(self):
         if self.remote:
-            r = requests.delete(self.proxy + '/api/process/session/' + self.bank + '/' + self.session)
-            if not r.status_code == 200:
-                raise Exception('Failed to connect to the download proxy')
+            for i in range(3):
+                try:
+                    url = self.proxy + '/api/process/session/' + self.bank + '/' + self.session
+                    r = requests.delete(url)
+                    if r.status_code == 200:
+                        return
+                except Exception:
+                    logging.exception('Failed to send clean operation: %s' % (url))
+            raise Exception('Failed to connect to the process proxy')
